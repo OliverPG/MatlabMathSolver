b9c2013e3184ab96755457185e2f2a8979ec6dcb
OliverNHY <38200298+OliverNHY@users.noreply.github.com>
2020-07-08 19:10:38 +0800

    temp version
    temp version

--- GaussCosFit/GaussCos_estimateGaussFit.m
+++ GaussCosFit/GaussCos_estimateGaussFit.m
@@ -1,6 +1,6 @@
 clear;clc;
 % syms x y 
-% syms a1 a2 a3 a4 a5 f
+% syms a1 a2 a3 a4 a5 f00
 % a=[a1,a2,a3,a4,a5];
 % f=a1*exp(a2*x^2+a3*x)*cos(a4*x)+a5-y;
 % f=a1+a2+a3+a4+a5;
@@ -9,22 +9,17 @@ clear;clc;
 % end
 fre=[300;470;640;725;810;980;1150;1320;1490;1575;1660;1830;2000];
 rcs=[3.50266273829231;7.18167481560552;8.29853604322728;8.27168802473965;7.23349089331251;4.78699634777701;-0.446225679851643;-10.2257061777637;-6.08043729857107;-3.19274369835503;-1.62180305353850;-0.425360901742028;-1.50920384069627];
-fsHd=@(x)sum((x(1).*exp(-1./x(2).*(fre-x(3)).^2).*cos(x(4).*fre)+x(5)-rcs).^2);
-% fs=subs(f,{x,y},{fre,rcs});
-% fs=subs(fs,a,)
-% evalF=
-% funcHd=matlabFunction(fs);
-% funcHd=matlabFunction(fs,'vars',num2cell(a));
-% options = optimoptions('particleswarm','display','off');
-% funcHd1=matlabFunction(sum(fs.^2),'vars',a);
-% [x0,fval,exitFlag]=particleswarm(fsHd,5,-inf(1,5),inf(1,5),options)
-[x0,fval,exitFlag]=GlobalSolve(fsHd,5)
-plot(fre,rcs,'.')
+a7Esti=mean(rcs);
+gaussY=rcs-a7Esti;
+gausHd=@(x)sum((x(1).*exp(-x(2).*(fre-x(3)).^2)+x(4)).^2);
+% fsHd=@(x)sum((x(1).*exp(-1./x(2).*(fre-x(3)).^2).*cos(x(4).*fre)+x(5)-rcs).^2);
+[x0,fval,exitFlag]=GlobalSolve(gausHd,4)
+plot(fre,gaussY,'.')
 syms xx
-ty=x0(1).*exp(-x0(2).*(xx-x0(3)).^2).*cos(x0(4).*xx)+x0(5);
+ty=x0(1).*exp(-x0(2).*(xx-x0(3)).^2)+x0(4);
 %%
 % ty=10.*exp(-1/4e5.*(xx+200).^2).*cos(0.5/pi.*(xx+150)./10)+5;%;
-hold on;plotFuncOn(300,200,ty,1);
+hold on;plotFuncOn(300,2000,ty,1);
 %%
 % x0 = particleswarm(funcHd1,5,-inf(1,5),inf(1,5),options);
 % GlobalSolve(funcHd,5)
