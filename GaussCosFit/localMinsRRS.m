6916e32cac00c320ec54615e34758b8397569cbb
OliverNHY <38200298+OliverNHY@users.noreply.github.com>
2020-08-31 21:31:03 +0800

    Update localMinsRRS.m
    add z position compare function and point combine

--- GaussCosFit/localMinsRRS.m
+++ GaussCosFit/localMinsRRS.m
@@ -55,8 +55,8 @@ bdLim=[bdLim(:,1:nVar-1),[-Inf;Inf]];
 rpics=2;cpics=2;
 fig=figure;
 subplot(rpics,cpics,1);
+x1v=linspace(bdLim(1,1),bdLim(2,1),100);
 if nVar>=3
-    x1v=linspace(bdLim(1,1),bdLim(2,1),100);
     x2v=linspace(bdLim(1,2),bdLim(2,2),100);
     [x1Grid,x2Grid]=ndgrid(x1v,x2v);
     xyuGridCell=[{x1Grid},{x2Grid}];
@@ -71,7 +71,7 @@ else % nVar==2
     plot(x1v,zGrid,'--','LineWidth',0.5,'color','black');hold on;
 end
 err=1e-20;% zero
-vError=1e-10;% zero for velocity
+% vError=1e-10;% zero for velocity
 dzError=1e-3;% zero for dz
 nDim=nVar;% motion dimision or number of variates
 dt=0.05;% time evolution factor
@@ -83,28 +83,29 @@ GVec=GVec.*m0*g;
 sVec=jacobian(fFunc,varsList);
 sBV=sVec./norm(sVec);
 sBVFunc=matlabFunction(sBV);varBvList=symvar(sBV);varBvListStr=string(varBvList);
-[BvIndex,varsIndex]=find(varsListStr==varBvListStr');nVarBv=length(varBvList);
+[~,varsIndex]=find(varsListStr==varBvListStr');nVarBv=length(varBvList);
 
-nPoints=200;
+nPoints=100;
 nPerDim=floor(nPoints^(1/(nDim-1)));
-nRand=200-nPerDim^(nDim-1);
+nTotalDim=nPerDim^(nDim-1);
+nRand=nPoints-nTotalDim;
 nFlys=40;
 activePList=1:nPoints;
-
+activePListLen=length(activePList);
 % pos=rand(nPoints,nDim);% Each row as the position of a point(random produce)
 seedR=linspace(0,1,nPerDim);
 ndGridPara=mat2cell(repmat(seedR',1,nDim-1),nPerDim,ones(1,nDim-1));
 gridCell=cell(1,nDim-1);[gridCell{:}]=ndgrid(ndGridPara{:});
-gridMat=[];
+gridMat=NaN(nTotalDim,nDim-1);
 for iGrid=1:nDim-1
-    matCache=gridCell{iGrid};
-    gridMat=[gridMat,matCache(:)];
+%     matCache=gridCell{iGrid};
+    gridMat(:,iGrid)=reshape(gridCell{iGrid},nTotalDim,1);
 end
 pos=[gridMat;rand(nRand,nDim-1)];% Each row as the position of a point
 pos=[pos,zeros(nPoints,1)];
 
 pos=repmat(bdLim(1,:),nPoints,1)+repmat(bdLim(2,:)-bdLim(1,:),nPoints,1).*pos;
-posActiCell_z=mat2cell(pos(activePList,1:nDim-1),length(activePList),ones(1,nDim-1));
+posActiCell_z=mat2cell(pos(activePList,1:nDim-1),activePListLen,ones(1,nDim-1));
 pos(activePList,end)=zFunc(posActiCell_z{:});% Initial positions on surface
 GVec=repmat(GVec,nPoints,1);
 vmVec=zeros(nPoints,nDim);
@@ -113,47 +114,48 @@ dtVec=ones(nPoints,1).*dt;
 Ndz=10;dzMat=ones(nPoints,Ndz).*10;
 stopFlag=0;
 count=0;
-while stopFlag==0 && count<40 || count<=Ndz      
-    % Points falling    
-%     vmVec=zeros(nPoints,nDim);
-%     vmVnorm=zeros(nPoints,nDim)+err;
+while stopFlag==0 && count<100 || count<=Ndz
+    % Points falling
+    %     vmVec=zeros(nPoints,nDim);
+    %     vmVnorm=zeros(nPoints,nDim)+err;
     
-    posActiCell_BV=mat2cell(pos(activePList,varsIndex),length(activePList),ones(1,nVarBv));
+    activePListLen=length(activePList);
+    posActiCell_BV=mat2cell(pos(activePList,varsIndex),activePListLen,ones(1,nVarBv));
     sBV_v=sBVFunc(posActiCell_BV{:});
-    FgpVec(activePList,:)=GVec(activePList,:)-repmat(dot(GVec(activePList,:),sBV_v,2),1,nDim).*sBV_v;
+    FgpVec=NaN(nPoints,nDim);FgpVec(activePList,:)=GVec(activePList,:)-repmat(dot(GVec(activePList,:),sBV_v,2),1,nDim).*sBV_v;
     fVec=zeros(size(FgpVec));
     fVec(activePList,:)=-abs(repmat(dot(GVec(activePList,:),sBV_v(:,:),2),1,nDim)).*vmVec(activePList,:)./vmVnorm(activePList,:).*mu;
-    aVec(activePList,:)=(FgpVec(activePList,:)+fVec(activePList,:))./m0;
-    dvVec(activePList,:)=aVec(activePList,:).*dtVec(activePList,:);
+    aVec=NaN(nPoints,nDim);aVec(activePList,:)=(FgpVec(activePList,:)+fVec(activePList,:))./m0;
+    dvVec=NaN(nPoints,nDim);dvVec(activePList,:)=aVec(activePList,:).*dtVec(activePList,:);
     vmVec(activePList,:)=vmVec(activePList,:)+dvVec(activePList,:);vmVec(activePList,end)=0;
     vmVnorm=vecnorm(vmVec,2,2);vmVnorm=repmat(vmVnorm,1,nDim)+err;
-    rVec(activePList,:)=vmVec(activePList,:).*dtVec(activePList,:);
+    rVec=NaN(nPoints,nDim);rVec(activePList,:)=vmVec(activePList,:).*dtVec(activePList,:);
     flySeq=0:nFlys-1;flySeq=reshape(flySeq,1,1,nFlys);
     rVecFlys=rVec.*flySeq;
     posRep=repmat(pos,1,1,nFlys);
     posRep(activePList,:,:)=posRep(activePList,:,:)+rVecFlys(activePList,:,:);
-    bdLoRep=repmat(bdLim(1,1:nDim-1),length(activePList),1,nFlys);%%boundary Check
-    bdUpRep=repmat(bdLim(2,1:nDim-1),length(activePList),1,nFlys);
+    bdLoRep=repmat(bdLim(1,1:nDim-1),activePListLen,1,nFlys);%%boundary Check
+    bdUpRep=repmat(bdLim(2,1:nDim-1),activePListLen,1,nFlys);
     bdOutUpIndex=find(posRep(activePList,1:nDim-1,:)>bdUpRep);
     bdOutLoIndex=find(posRep(activePList,1:nDim-1,:)<bdLoRep);
     if ~isempty(bdUpRep)
-        bdUpRepNd=linerIndex2Nd(bdOutUpIndex,[length(activePList),nDim,nFlys]);
+        bdUpRepNd=linerIndex2Nd(bdOutUpIndex,[activePListLen,nDim,nFlys]);
         posRep(activePList(bdUpRepNd(:,1)),1:nDim-1,bdUpRepNd(:,3))=bdUpRep(bdUpRepNd(:,1),1:nDim-1,bdUpRepNd(:,3));
     end
     if ~isempty(bdLoRep)
-        bdLoRepNd=linerIndex2Nd(bdOutLoIndex,[length(activePList),nDim,nFlys]);
+        bdLoRepNd=linerIndex2Nd(bdOutLoIndex,[activePListLen,nDim,nFlys]);
         posRep(activePList(bdLoRepNd(:,1)),1:nDim-1,bdLoRepNd(:,3))=bdLoRep(bdLoRepNd(:,1),1:nDim-1,bdLoRepNd(:,3));
     end
-    posRepActiCell_z=mat2cell(posRep(activePList,1:nDim-1,:),length(activePList),ones(1,nDim-1),nFlys);
-    posZFlys=reshape(zFunc(posRepActiCell_z{:}),length(activePList),nFlys);
+    posRepActiCell_z=mat2cell(posRep(activePList,1:nDim-1,:),activePListLen,ones(1,nDim-1),nFlys);
+    posZFlys=reshape(zFunc(posRepActiCell_z{:}),activePListLen,nFlys);
     [zMins,zMinsIndex]=min(posZFlys,[],2);
     zMinsIndexE1=find(zMinsIndex==1);
     if ~isempty(zMinsIndexE1)
         vmVec(activePList(zMinsIndexE1),:)=zeros(length(zMinsIndexE1),nDim);
     end
         
-    xyMinsPos=[];
-    for index=1:length(activePList)
+    xyMinsPos=NaN(activePListLen,nDim-1);
+    for index=1:activePListLen
         xyMinsPos(index,:)=posRep(activePList(index),1:nDim-1,zMinsIndex(index));
     end
     oldPos=pos;
@@ -176,15 +178,26 @@ while stopFlag==0 && count<40 || count<=Ndz
     else %nvar=2
         handleS=scatter(pos(:,1),pos(:,2),'.');title('Position');
     end
-    subplot(rpics,cpics,2);hold on;scatter(repmat(count,nPoints,1),pos(:,end),'.');title('z');
+    subplot(rpics,cpics,2);hold on;scatter(repmat(count,nPoints,1),pos(:,end),'.');title(['z',string(max(pos(:,end)))]);
     if count>Ndz
-        subplot(rpics,cpics,3);hold on;scatter(repmat(count,nPoints,1),dzMatAver,'.');title('dzA');
+        subplot(rpics,cpics,3);hold on;scatter(repmat(count,nPoints,1),dzMatAver,'.');title(['dzA',string(max(dzMatAver))]);
     end
-    subplot(rpics,cpics,4);hold on;scatter(repmat(count,length(activePList),1),activePList,'.');title('activePList');
+    subplot(rpics,cpics,4);hold on;scatter(repmat(count,activePListLen,1),activePList,'.');title(['activePList',string(activePListLen)]);
     drawnow;
-    
+    %z and position compare
+    errComp=1;
+    [~,indexSortPos]=sort(pos(activePList,end));
+    sortPos=pos(activePList(indexSortPos),:);
+    dPos=sortPos(1:end-1,:)-sortPos(2:end,:);    
+    indexCompA=find(vecnorm(dPos,2,2)<errComp);
+    indexComp=[];
+    if ~isempty(indexCompA)
+        indexComp=activePList(indexSortPos(indexCompA));
+        indexComp=reshape(indexComp,length(indexComp),1);
+    end
+    %convergence condition
     dzA0Index=find(abs(dzMatAver)<dzError);
-    stopIndex=unique(dzA0Index);
+    stopIndex=unique([dzA0Index;indexComp]);
     if ~isempty(stopIndex)
         activePList=setdiff(activePList,stopIndex);
     end
